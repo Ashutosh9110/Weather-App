245dfd9dc9200144ed158577421d1fa6664d2e64
Ashutosh9110 <ashusingh19911082@gmail.com>
2024-12-11 15:07:39 +0530

    final commit

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,36 +1,67 @@
-import { API_CONFIG } from "./config"
-import { Coordinates, WeatherData } from "./types";
+import { API_CONFIG } from "./config";
+import type {
+  WeatherData,
+  ForecastData,
+  GeocodingResponse,
+  Coordinates,
+} from "./types";
 
-class WEATHERAPI {
-    private createUrl(endpoint: string, params: Record <string, string | number>) {
-        const searchParams = new URLSearchParams({
-            appid : API_CONFIG.API_KEY, 
-            ...params,
-        })
-        return `${endpoint} ? ${ searchParams.toString()}`
-    } // this private function cannot be accessed from outside this adds a layer of privacy to our code.}
-// we have two endpoints..baseUrl and GEO in config.ts
-// Recod types means that "params" will be an object with "key" as string and "value" can either be string or a number
+class WeatherAPI {
+  private createUrl(endpoint: string, params: Record<string, string | number>) {
+    const searchParams = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+    return `${endpoint}?${searchParams.toString()}`;
+  }
 
-    private async fetchData <T> (url: string): Promise <T> { // here <T> means that the type can be of a dynamic type
-        const response = await fetch(url);
-        if(!response.ok) {
-            throw new Error(`Weather API Error: ${response.statusText}`)
-        }
-        return response.json()
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Weather API Error: ${response.statusText}`);
     }
 
-    async getCurrentWeather({ lat, lon } : Coordinates): Promise <WeatherData> {
-        const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
-            lat: lat.toString(),
-            lon: lon.toString(),
-            units: API_CONFIG.DEFAULT_PARAMS.units,
-        });
+    return response.json();
+  }
 
-        return this.fetchData<WeatherData>(url)
-    }
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: "metric",
+    });
+    return this.fetchData<WeatherData>(url);
+  }
 
-    async getForecast() {}
+  async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: "metric",
+    });
+    return this.fetchData<ForecastData>(url);
+  }
 
-    async reverseGeocode() {}
+  async reverseGeocode({
+    lat,
+    lon,
+  }: Coordinates): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      limit: "1",
+    });
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+
+  async searchLocations(query: string): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/direct`, {
+      q: query,
+      limit: "5",
+    });
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
 }
+
+export const weatherAPI = new WeatherAPI();
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,18 @@
-import type { PropsWithChildren } from 'react'
-import Header from './header'
+import type { PropsWithChildren } from "react";
+import { Header } from "./header";
 
-const Layout = ({ children }: PropsWithChildren) => {
+export function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='bg-gradient-to-br from-background to-muted'>
-        <Header /> 
-        <main className='min-h-screen container mx-auto px-4 py-8'>
+    <div className=" bg-gradient-to-br from-background to-muted">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
-        </main>
-          <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/'>
-            <div className='container mx-auto px-4 text-center text-gray-800'>
-              <p>Sab moh maya hai</p>
-            </div>
-          </footer>
-        footer
+      </main>
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+        <div className="container mx-auto px-4 text-center text-gray-200">
+          <p>Sab moh maya hai</p>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
-
-export default Layout
